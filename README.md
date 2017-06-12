993c9eef2b41e38389ecc3befe75c38bd4210dcc
tatomyr <andrew.tatomyr@gmail.com>
2017-06-12 16:30:33 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Paired
+
+This library provides two main functions to transform an array of paired (key:val) objects to an appropriate object and vice-versa. 
---
+++ paired.js
@@ -0,0 +1,10 @@
+const paired = (customKey = 'key', customVal = 'val') => {
+  const arrToObj = arr => arr.reduce((prev, { [customKey]: key, [customVal]: val }) => ({ ...prev, [key]: val }), {});
+
+  const objToArr = obj => Object.keys(obj).map(key => ({ [customKey]: key, [customVal]: obj[key] }));
+
+  return {
+    arrToObj,
+    objToArr,
+  };
+}
