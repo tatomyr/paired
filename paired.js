69b20764a366e9bca9b291aedd24e0755a003001
tatomyr <andrew.tatomyr@gmail.com>
2017-06-17 12:54:38 +0300

    code restructured

--- paired.js
+++ paired.js
@@ -1,7 +1,13 @@
 const paired = (customKey = 'key', customVal = 'val') => {
-  const arrToObj = arr => arr.reduce((prev, { [customKey]: key, [customVal]: val }) => ({ ...prev, [key]: val }), {});
+  const arrToObj = arr => arr.reduce((
+    prev,
+    { [customKey]: key, [customVal]: val }
+  ) => ({ ...prev, [key]: val }), {});
 
-  const objToArr = obj => Object.keys(obj).map(key => ({ [customKey]: key, [customVal]: obj[key] }));
+  const objToArr = obj => Object.keys(obj).map(key => ({
+    [customKey]: key,
+    [customVal]: obj[key]
+  }));
 
   return {
     arrToObj,
